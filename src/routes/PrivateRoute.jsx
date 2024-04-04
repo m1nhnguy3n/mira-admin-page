8fc4feb9b23eab5c8e4c7058c0e228dc0fdb4287
VoVanKhoa <vankhoa.work@gmail.com>
2024-04-04 16:32:48 +0700

    feat:map7-pricing-rebase

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,22 +1,27 @@
-import Analytics from '../pages/Dashboard/Analytics';
-import Order from '../pages/Order/Order';
-import ProtectedRoute from './ProtectedRoute/ProtectedRoute';
+import Analytics from "../pages/Dashboard/Analytics";
+import Order from "../pages/Order/Order";
+import Pricing from "../pages/Pricing/Pricing";
+import ProtectedRoute from "./ProtectedRoute/ProtectedRoute";
 
 const PrivateRoute = [
-    {
-        path: '/',
-        element: <ProtectedRoute />,
-        children: [
-            {
-                path: 'analytic',
-                element: <Analytics />,
-            },
-            {
-                path: "order",
-                element: <Order />,
-            }
-        ],
-    },
+  {
+    path: "/",
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: "analytic",
+        element: <Analytics />,
+      },
+      {
+        path: "order",
+        element: <Order />,
+      },
+      {
+        path: "pricing",
+        element: <Pricing />,
+      },
+    ],
+  },
 ];
 
 export default PrivateRoute;
